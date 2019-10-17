0f1edf7902645d59eb218d854fe6f1e23ad508a5
龙雲 <2210994554@qq.com>
2019-10-17 20:45:31 +0800

    后端 longyun  LYmianbao728

--- ImageStyle/Image/settings.py
+++ ImageStyle/Image/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fn9(+3-(0dr!f*)m$(-eu3+!h%5fcvy)-qcjhzcghdzg&mmm=7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
